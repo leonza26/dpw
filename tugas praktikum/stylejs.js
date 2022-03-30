b7cd45856c4afeb4280701c0b81f6fe7b7675ac5
leonza26 <muhammadleonza10@gmail.com>
2022-03-30 23:33:26 +0700

    dpw

---
+++ tugas praktikum/stylejs.js
@@ -0,0 +1,31 @@
+const a = document.getElementsByTagName('a');
+    for (let i = 0; i < a.length; i++) {
+        a[i].style.color = 'green';
+    }
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.color = 'green';
+h1.style.backgroundColor = 'yellow';
+
+const p = document.getElementsByTagName('p');
+p[0].style.backgroundColor = 'green';
+p[0].style.color = 'yellow';
+
+const instagram = document.getElementsByClassName('instagram');
+instagram[0].style.backgroundColor = 'green';
+    
+const h2 = document.getElementsByTagName('h2');
+h2[0].style.color = 'white';
+h2[1].style.color = 'white';
+
+const p1 = document.querySelector('.instagram2 p');
+p1.style.color = 'red';
+
+const lia = document.querySelector('.li2 a');
+lia.style.color = 'white';
+
+const foter = document.getElementsByTagName('footer')[0];
+foter.style.color = 'green';
+foter.style.fontSize = '20px'
+
+const buton = document.querySelector('.instagram1 a button');
+buton.style.backgroundColor = 'white';
